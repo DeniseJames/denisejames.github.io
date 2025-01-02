46d1f30d361defba79d55ec93a9572ca6759f3a0
unknown <denisetoo@gmail.com>
2025-01-01 18:51:56 -0600

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: './', // Use relative paths for all assets
+  base: './', // Ensures relative paths for assets
 })
