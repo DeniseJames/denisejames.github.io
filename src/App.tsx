ee348e2d395991ccdee9de4e8e951c2f07df50cf
unknown <denisetoo@gmail.com>
2025-01-12 14:06:20 -0600

    add home and navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { HashRouter as Router } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import NavbarComponent from './components/NavBar/NavBarComponent';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const HomeComponent = React.lazy(() => import('./components/Home/HomeComponent'));
 
 function App() {
   return (
     <Router>
-      {/* Your routes */}
+      <NavbarComponent />
+      <div className="container-fluid">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomeComponent />} />
+            {/* Add other routes as needed */}
+          </Routes>
+        </Suspense>
+      </div>
     </Router>
   );
 }
